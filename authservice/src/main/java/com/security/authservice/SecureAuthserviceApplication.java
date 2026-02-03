65fdb93a75b45bdc2e52ebd2f377deba2efc47f2
NikhilKummari <nikhil62819@gmail.com>
2026-02-03 21:18:35 +0530

    Initial Spring Boot setup and project structure

---
+++ authservice/src/main/java/com/security/authservice/SecureAuthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.security.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAuthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAuthserviceApplication.class, args);
+	}
+
+}
